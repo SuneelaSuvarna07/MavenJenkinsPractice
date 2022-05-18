023135b2e23c5d6b9e068f5ed011193e90ac34d1
suneela <sunilsuvarna87@gmail.com>
2022-05-18 19:42:00 +0530

    assert

--- src/test/java/p1/AppiumTest.java
+++ src/test/java/p1/AppiumTest.java
@@ -1,27 +1,25 @@
 package p1;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class AppiumTest {
-	
+
 	@Test
-	public void createSession()
-	{
+	public void createSession() {
 		System.out.println("Appium Driver Started");
+		Assert.fail();
 	}
-	
+
 	@Test
-	public void testMobileLogin()
-	{
+	public void testMobileLogin() {
 		System.out.println("Logged into mobile app");
 	}
 
-	
 	@Test
-	public void testMobileLogout()
-	{
+	public void testMobileLogout() {
 		System.out.println("Logged out from mobile app");
 	}
 
